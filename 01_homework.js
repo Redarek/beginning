f0322d4e2bb3ba03ee3fec1f1efc5a1da0957c4d
Redarek <80126370+Redarek@users.noreply.github.com>
2021-03-08 21:49:46 +0300

    Update 01_homework.js
    функция выводи список чисел, а не одно число

--- 01_homework.js
+++ 01_homework.js
@@ -1,16 +1,15 @@
-function outputNumbers (a) {
-	switch (a) {
-		case 5: console.log('пять'); break
-		case 13: console.log('тринадцать'); break
-		case 22: console.log('двадцать два'); break
-		case 35: console.log('тридцать пять'); break
-		case 98: console.log('девяносто восемь'); break
-		default: console.log(a); break
+let i = 0
+function outputNumbers (n) {
+	while (i < n) {
+		switch (n) {
+			case 5: console.log('пять'); break
+			case 13: console.log('тринадцать'); break
+			case 22: console.log('двадцать два'); break
+			case 35: console.log('тридцать пять'); break
+			case 98: console.log('девяносто восемь'); break
+			default: console.log(n); break
+		}
+		i += 1
 	}
+	i = 0
 }
-outputNumbers(5)
-outputNumbers(13)
-outputNumbers(22)
-outputNumbers(35)
-outputNumbers(98)
-outputNumbers(1109)
