d51235b1f53e03d956484efafc790d2bcc11e3a8
Redarek <80126370+Redarek@users.noreply.github.com>
2021-03-09 19:45:47 +0300

    Update 01_homework.js

--- 01_homework.js
+++ 01_homework.js
@@ -1,13 +1,13 @@
 let i = 0
 function outputNumbers (n) {
 	while (i < n) {
-		switch (n) {
+		switch (i) {
 			case 5: console.log('пять'); break
 			case 13: console.log('тринадцать'); break
 			case 22: console.log('двадцать два'); break
 			case 35: console.log('тридцать пять'); break
 			case 98: console.log('девяносто восемь'); break
-			default: console.log(n); break
+			default: console.log(i); break
 		}
 		i += 1
 	}
